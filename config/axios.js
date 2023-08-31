519283aee66c387b7a7c81c36a529223aaad9dbb
Facundo Viale <91499618+GreyshadowARG@users.noreply.github.com>
2023-08-31 08:46:44 -0300

    nuevos estilos aplicados

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL= "http://localhost:8080/"
-//const BASE_URL= 'https://bethel-back-f07a-dev.fl0.io/'
+//const BASE_URL= "http://localhost:8080/"
+const BASE_URL= 'https://bethel-back-f07a-dev.fl0.io/'
 
 export default axios.create({
     baseURL: BASE_URL,
